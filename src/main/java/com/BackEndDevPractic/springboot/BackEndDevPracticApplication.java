e3e215cd37603a362181820cf7595dfc37660d17
AnguezdemTaha <anguezdemtaha16@gmail.com>
2022-03-19 10:49:54 +0100

    Initialize project

---
+++ src/main/java/com/BackEndDevPractic/springboot/BackEndDevPracticApplication.java
@@ -0,0 +1,13 @@
+package com.BackEndDevPractic.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndDevPracticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndDevPracticApplication.class, args);
+	}
+
+}
